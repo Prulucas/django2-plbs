993d41e6eaa1ff88106bc2c99aee08002cd6e80f
Prulucas <plbrandsilva@gmail.com>
2026-04-02 13:11:35 -0300

    corrigindo bugs

--- django2/settings.py
+++ django2/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # media vai criar os diretorios do dados salvos em bd
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- django2/wsgi.py
+++ django2/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for django2 project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+# Importa as ferramentas de arquivos estáticos e mídia
 from dj_static import Cling, MediaCling
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django2.settings')
 
+# O Cling cuida do STATIC e o MediaCling cuida do MEDIA (fotos dos consoles)
 application = Cling(MediaCling(get_wsgi_application()))
