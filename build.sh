02ef405d8b60efc2ba5cc988265135130e329c64
Prulucas <plbrandsilva@gmail.com>
2026-04-02 14:08:24 -0300

    adição de superusuário

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
-# Interrompe a execução se houver erro em qualquer comando
 set -o errexit
 
-# 1. Instala as dependências (Django, gunicorn, dj-static, etc)
 pip install -r requirements.txt
-
-# 2. Reúne o CSS/JS na pasta 'staticfiles' para o dj-static ler
 python manage.py collectstatic --no-input
-
-# 3. Cria as tabelas no banco de dados PostgreSQL do Render
 python manage.py migrate
+
+# Adicione esta linha (ajuste o nome, email e senha)
+# Ela verifica se o usuário já existe antes de tentar criar
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('Pedro', 'plbrandsilva@email.com', 'Pl@23107')"
