7ef87589ec76db20410db08571138c664f951fce
Prulucas <plbrandsilva@gmail.com>
2026-04-02 14:26:24 -0300

    teste

--- django2/wsgi.py
+++ django2/wsgi.py
@@ -1,9 +1,8 @@
 import os
 from django.core.wsgi import get_wsgi_application
-# Importa as ferramentas de arquivos estáticos e mídia
 from dj_static import Cling, MediaCling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django2.settings')
 
-# O Cling cuida do STATIC e o MediaCling cuida do MEDIA (fotos dos consoles)
+# Garanta que não existam parênteses sobrando ou faltando
 application = Cling(MediaCling(get_wsgi_application()))
