5502f3a4f60413ba70466c8e618db36f105ba7ff
Prulucas <plbrandsilva@gmail.com>
2026-04-02 14:38:23 -0300

    Fix: Forçando ordem de execução no script admin

--- create_admin.py
+++ create_admin.py
@@ -2,20 +2,26 @@ from django.contrib.auth.models import User
 import os
 import django
 
-# 1. Configura o módulo de settings PRIMEIRO
+# 1. DEFINE AS CONFIGURAÇÕES PRIMEIRO
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django2.settings')
 
-# 2. Inicializa o Django
+# 2. INICIALIZA O DJANGO
 django.setup()
 
-# 3. SÓ AGORA importa os modelos
+# 3. SÓ DEPOIS DISSO IMPORTA O MODELO (Dentro da lógica)
 
-username = 'Pedro'
-email = 'plbrandsilva@email.com'
-password = 'Pl@23107'
 
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password)
-    print(f"Usuario {username} criado com sucesso!")
-else:
-    print(f"Usuario {username} ja existe.")
+def create_user():
+    username = 'Pedro'
+    email = 'plbrandsilva@email.com'
+    password = 'Pl@23107'
+
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username, email, password)
+        print(f"Usuario {username} criado com sucesso!")
+    else:
+        print(f"Usuario {username} ja existe.")
+
+
+if __name__ == "__main__":
+    create_user()
